2661ca5f6997013b6b126ad13ea0f6434b27bc36
ches0703 <ches0703@naver.com>
2025-04-16 13:34:14 +0900

    feat : FeedHomeScreen 추가

--- src/navigations/drawer/MainDrawerNavigator.tsx
+++ src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MapHomeScreen from '../../screens/MapHomeScreen';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import FeedHomeScreen from '../../screens/FeedHomeScreen';
 const Drawer = createDrawerNavigator();
 
 const MainDrawerNavigator = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="MapHome" component={MapHomeScreen} />
+      <Drawer.Screen name="FeedHome" component={FeedHomeScreen} />
     </Drawer.Navigator>
   );
 };
