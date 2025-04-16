8311d045843985df9deb2d89b78dbfb7e34d9966
ches0703 <104610261+ches0703@users.noreply.github.com>
2025-04-16 15:45:07 +0900

    [Chore] React Nativr & React Navigation 설치 및 설정 (#3)
    * chore : react-navigation 설치
    * feat : drawer navigator 적용
    * feat : FeedHomeScreen 추가
    * feat : CalendarHomeScreen 추가
    * feat : SignupScreen 추가
    * feat : StackNavigator 설정 수정
    * reafactor : 디렉터리 분리

---
+++ src/screens/auth/AuthHomeScreen.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Button, SafeAreaView, Text} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {authNavigations} from '../../constants';
+import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+
+type AuthHomeScreenProps = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME | typeof authNavigations.SIGNUP
+>;
+
+const AuthHomeScreen = ({navigation}: AuthHomeScreenProps) => {
+  return (
+    <SafeAreaView>
+      <Text>AuthHomeScreen</Text>
+      <Button
+        title="로그인 화면으로 이동"
+        onPress={() => navigation.navigate(authNavigations.LOGIN)}
+      />
+      <Button
+        title="회원가입 화면으로 이동"
+        onPress={() => navigation.navigate(authNavigations.SIGNUP)}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default AuthHomeScreen;
---
+++ src/screens/map/MapHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const MapHomeScreen = () => {
+  return (
+    <View>
+      <Text>MapHomeScreen</Text>
+    </View>
+  );
+};
+
+export default MapHomeScreen;
